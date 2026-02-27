0ea58b95ed52b6df3237ad682279cfb00a81679a
MD-shahriar6 <shahriar2005january@gmail.com>
2026-02-27 17:42:17 +0600

    Add multiply function

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 print("MD. shahriar parvez! ")
 print("02/27/2026")
 
-from utils import add, subtract
+from utils import add, subtract, multiply
 
 print("Add:", add(10, 5))
 print("Subtract:", subtract(10, 5))
+print("Multiply:", multiply(10, 5))
